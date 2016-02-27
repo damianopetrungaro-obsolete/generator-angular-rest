911c12422db7804c1d659a5e42ba270f0e95096d
Damiano Petrungaro <damianopetrungaro@gmail.com>
2016-02-27 18:58:02 +0100

    Before replacer

--- generators/module/index.js
+++ generators/module/index.js
@@ -3,26 +3,30 @@ var helper = require('../../helper.js');
 
 console.log(helper);
 
-// module.exports = generators.Base.extend({
-//   // The name `constructor` is important here
-//   constructor: function () {
-//     // Calling the super constructor is important so our generator is correctly set up
-//     generators.Base.apply(this, arguments);
-//
-//     // This makes `appname` a required argument.
-//     this.argument('moduleName', { type: String, required: true });
-//
-//   },
-//   move: function () {
-//
-//     this.fs.copyTpl(
-//       this.templatePath(),
-//       this.destinationPath(helper.MODULES_PATH + this.moduleName), {
-//
-//         modulename: this.moduleName,
-//         modulenameCapitalize: exports.capitalize(this.moduleName),
-//         appname: this.config.get('appName')
-//       }
-//     );
-//   }
-// });
+module.exports = generators.Base.extend({
+  // The name `constructor` is important here
+  constructor: function () {
+    // Calling the super constructor is important so our generator is correctly set up
+    generators.Base.apply(this, arguments);
+
+    // This makes `appname` a required argument.
+    this.argument('moduleName', { type: String, required: true });
+
+  },
+  move: function () {
+
+    this.fs.copyTpl(
+      this.templatePath(),
+      this.destinationPath(helper.MODULES_PATH + this.moduleName), {
+
+        modulename: this.moduleName,
+        modulenameCapitalize: helper.capitalize(this.moduleName),
+        appname: this.config.get('appName')
+      }
+    );
+
+    // Rename all files created
+    helper.replacer('{module}', this.moduleName, helper.MODULES_PATH);
+
+  }
+});
--- helper.js
+++ helper.js
@@ -1,9 +1,20 @@
-//Path
+var replace = require('replace');
 
+//Path
 exports.MODULES_PATH = 'src/app/modules/';
 exports.COMPONENTS_PATH = 'src/app/shared/components/';
 
 //Capitalize
 exports.capitalize = function (string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+exports.replacer = function (from, to, path) {
+  replace({
+    regex: from,
+    replacement: to,
+    paths: [path],
+    recursive: true,
+    silent: true,
+  });
 };
